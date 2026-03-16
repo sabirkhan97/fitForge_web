f08701ffeeb17a6a57e1e4b120ded474465332fc
sabirKhan97 <khansabir5400@gmail.com>
2026-03-16 18:00:37 +0530

    remove tailwind vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
+// import { defineConfig } from "vite"
+// import react from "@vitejs/plugin-react"
+// import tailwindcss from "@tailwindcss/vite"
+// import path from "path"
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
